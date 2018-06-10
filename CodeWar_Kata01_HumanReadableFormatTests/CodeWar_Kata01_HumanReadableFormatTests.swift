d70871ea6a9b0aecd923955cd4e8fb1a1cfddccf
AKI <nobodyyu@gmail.com>
2018-06-10 23:13:35 +0800

    Test OneDayAndOneMinute Pass

--- CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
+++ CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
@@ -109,6 +109,10 @@ class CodeWar_Kata01_HumanReadableFormatTests: XCTestCase {
         XCTAssertEqual(HumanReadableDateFormat(3600*24+2), "1 Day and 2 Seconds")
     }
 
+    func test223_OneDayAndOneMinute() {
+        XCTAssertEqual(HumanReadableDateFormat(3600*24+60), "1 Day and 1 Minute")
+    }
+
     
    
 
