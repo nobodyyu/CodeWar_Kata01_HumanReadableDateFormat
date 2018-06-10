f8b8e8bd83409f6799357e39343ded913fb8b6d7
AKI <nobodyyu@gmail.com>
2018-06-10 23:24:58 +0800

    OneYear Pass

--- CodeWar_Kata01_HumanReadableFormat/HumanReadableDateFormat.swift
+++ CodeWar_Kata01_HumanReadableFormat/HumanReadableDateFormat.swift
@@ -7,15 +7,18 @@
 //
 
 func HumanReadableDateFormat(_ orginalSecond:Int) -> String {
-    
-    let Day = (orginalSecond/60/60/24)
-    var secondsCalculate = orginalSecond % (60*60*24)
+
+    let Year = (orginalSecond/60/60/24/365)
+    var secondsCalculate = orginalSecond % (60*60*24*365)
+    let Day = (secondsCalculate/60/60/24)
+    secondsCalculate = secondsCalculate % (60*60*24)
     let Hour = (secondsCalculate/60/60)
     secondsCalculate = secondsCalculate % (60*60)
     let Minute = (secondsCalculate/60)
     secondsCalculate = secondsCalculate % 60
     let Second = secondsCalculate
-    
+
+    var resultYears = ""
     var resultDays = ""
     var resultHours = ""
     var resultMinutes = ""
@@ -26,10 +29,12 @@ func HumanReadableDateFormat(_ orginalSecond:Int) -> String {
     resultMinutes = FormatFactory(Minute,"Minute")
     resultHours = FormatFactory(Hour,"Hour")
     resultDays = FormatFactory(Day,"Day")
+    resultYears = FormatFactory(Year,"Year")
     
     resultFinal = Conjunction(resultMinutes,resultSecond,false)
     resultFinal = Conjunction(resultHours,resultFinal,resultFinal.contains("and"))
     resultFinal = Conjunction(resultDays,resultFinal,resultFinal.contains("and"))
+    resultFinal = Conjunction(resultYears,resultFinal,resultFinal.contains("and"))
     
     return resultFinal == "" ? "0 Second" : resultFinal
 }
