b8660a4fc6c7671343cda69f7ec9e88f27eafc9f
AKI <nobodyyu@gmail.com>
2018-06-10 22:27:19 +0800

    Pass OneMinute

--- CodeWar_Kata01_HumanReadableFormat/HumanReadableDateFormat.swift
+++ CodeWar_Kata01_HumanReadableFormat/HumanReadableDateFormat.swift
@@ -7,5 +7,30 @@
 //
 
 func HumanReadableDateFormat(_ orginalSecond:Int) -> String {
-    return "\(orginalSecond) Second\((orginalSecond > 1 ? "s" : ""))"
+    
+    let Minute = (orginalSecond/60)
+    let Second = orginalSecond % 60
+    
+    var resultMinutes = ""
+    var resultSecond = ""
+    var resultFinal = ""
+    var conjunctionMinSec = ""
+    
+    if Minute == 0 {
+        resultSecond = "\(Second) Second\((Second > 1 ? "s" : ""))"
+        resultMinutes = ""
+        
+        resultFinal = "\(resultSecond)"
+    }else{
+        if Second != 0 {
+            resultSecond = "\(Second) Second\((Second > 1 ? "s" : ""))"
+            conjunctionMinSec = " and "
+        }
+        resultMinutes = "\(Minute) Minute\((Minute > 1 ? "s" : ""))"
+
+        resultFinal = "\(resultMinutes)\(conjunctionMinSec)\(resultSecond)"
+    }
+    
+    
+    return resultFinal
 }
