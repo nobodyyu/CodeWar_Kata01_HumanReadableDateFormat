228fe08581a25cbc2f21c8b69be5e2b3eb0c8461
AKI <nobodyyu@gmail.com>
2018-06-10 22:44:35 +0800

    Test OneHour Failed

--- CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
+++ CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
@@ -57,7 +57,10 @@ class CodeWar_Kata01_HumanReadableFormatTests: XCTestCase {
         XCTAssertEqual(HumanReadableDateFormat(122), "2 Minutes and 2 Seconds")
     }
     
-    
+    func test10_OneHour() {
+        XCTAssertEqual(HumanReadableDateFormat(3600), "1 Hour")
+    }
+
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
