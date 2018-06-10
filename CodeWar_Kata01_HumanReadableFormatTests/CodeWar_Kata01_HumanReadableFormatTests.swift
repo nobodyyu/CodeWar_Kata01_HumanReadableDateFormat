feceac4683239d41a09fa2d5891b238b904abce2
AKI <nobodyyu@gmail.com>
2018-06-10 23:01:56 +0800

    Test OneHourOneMinuteAndOneSecond Pass

--- CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
+++ CodeWar_Kata01_HumanReadableFormatTests/CodeWar_Kata01_HumanReadableFormatTests.swift
@@ -73,6 +73,10 @@ class CodeWar_Kata01_HumanReadableFormatTests: XCTestCase {
         XCTAssertEqual(HumanReadableDateFormat(3600+60), "1 Hour and 1 Minute")
     }
     
+    func test13_OneHourOneMinuteAndOneSecond() {
+        XCTAssertEqual(HumanReadableDateFormat(3600+60+1), "1 Hour, 1 Minute and 1 Second")
+    }
+    
    
 
     
